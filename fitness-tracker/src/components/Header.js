b162020f06232508be96f3f36cf37e1dabce1c90
Travis Saulat <tjsaulat@gmail.com>
2023-02-09 16:18:13 -0500

    Update Header.js

---
+++ fitness-tracker/src/components/Header.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Header = () => {
+
+    return (
+        <div>
+            <h1 className="display-2">Fitness Tracker</h1>
+
+            <nav className="navbar navbar-expand-lg bg-light">
+                <div className="container-fluid">
+
+                    <Link className="navbar-brand" to="/" >Home</Link>
+
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+
+                    <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+                    <div className="navbar-nav">
+
+                        <Link className="nav-link" to="/Login">Login</Link>
+
+                        <Link className="nav-link" to="/Signup">Signup</Link>
+                    </div>
+                    </div>
+                </div>
+            </nav>
+
+        </div>
+    );
+
+
+};
+
+export default Header;
