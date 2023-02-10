5663ba3864f07e0761dfcffe0b73cede0045066a
charinafaye7 <charinafaye7@hotmail.com>
2023-02-10 09:56:44 -0800

    Edited controller files and added exceptions

--- src/main/java/com/cognixia/jump/controller/UserAppController.java
+++ src/main/java/com/cognixia/jump/controller/UserAppController.java
@@ -1,18 +1,22 @@
 package com.cognixia.jump.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.cognixia.jump.exception.ResourceNotFoundException;
 import com.cognixia.jump.model.UserApp;
 import com.cognixia.jump.repository.UserAppRepository;
-
 
 @RequestMapping("/api")
 @RestController
@@ -37,13 +41,31 @@ public class UserAppController {
 		return ResponseEntity.status(201).body(created);
 	}
 	
+	@DeleteMapping("/user/{id}")
+	public ResponseEntity<?> deleteUser(@PathVariable int id) throws ResourceNotFoundException {
+		
+		Optional<UserApp> found = repo.findById(id);
+		
+		if(found.isEmpty()) {
+			throw new ResourceNotFoundException("User with id " + id + " was not found");
+		}
+		
+		repo.deleteById(id);
+		
+		return ResponseEntity.status(200).body(found.get());
+	}
 	
-	
-	
+	@PutMapping("/user/update")
+	public ResponseEntity<?> updateUser(@RequestBody UserApp user) throws ResourceNotFoundException {
+		
+		if(repo.existsById(user.getUser_id())) {
+			
+			UserApp updated = repo.save(user);
+			
+			return ResponseEntity.status(200).body(updated);
+		}
+		
+		throw new ResourceNotFoundException("Workout session id " + user.getUser_id() + " was not found");
+	}
 	
 }
-
-
-
-
-
--- src/main/java/com/cognixia/jump/controller/WorkoutSessionController.java
+++ src/main/java/com/cognixia/jump/controller/WorkoutSessionController.java
@@ -1,18 +1,22 @@
 package com.cognixia.jump.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.cognixia.jump.exception.ResourceNotFoundException;
 import com.cognixia.jump.model.WorkoutSession;
 import com.cognixia.jump.repository.WorkoutSessionRepository;
-
 
 @RequestMapping("/api")
 @RestController
@@ -26,7 +30,7 @@ public class WorkoutSessionController {
 	}
 	
 	@PostMapping("/workout")
-	public ResponseEntity<WorkoutSession> addUser(@RequestBody WorkoutSession workoutSession) {
+	public ResponseEntity<WorkoutSession> addWorkout(@RequestBody WorkoutSession workoutSession) {
 		
 		System.out.println(workoutSession);
 		
@@ -37,8 +41,31 @@ public class WorkoutSessionController {
 		return ResponseEntity.status(201).body(created);
 	}
 	
+	@DeleteMapping("/workout/{id}")
+	public ResponseEntity<?> deleteWorkout(@PathVariable int id) throws ResourceNotFoundException {
+		
+		Optional<WorkoutSession> found = repo.findById(id);
+		
+		if(found.isEmpty()) {
+			throw new ResourceNotFoundException("Workout session id " + id + " was not found");
+		}
+		
+		repo.deleteById(id);
+		
+		return ResponseEntity.status(200).body(found.get());
+	}
 	
-	
-	
-	
+	@PutMapping("/workout/update")
+	public ResponseEntity<?> updateProduct(@RequestBody WorkoutSession session) throws ResourceNotFoundException {
+		
+		if(repo.existsById(session.getSession_id())) {
+			
+			WorkoutSession updated = repo.save(session);
+			
+			return ResponseEntity.status(200).body(updated);
+		}
+		
+		throw new ResourceNotFoundException("Workout session id " + session.getSession_id() + " was not found");
+	}
+		
 }
