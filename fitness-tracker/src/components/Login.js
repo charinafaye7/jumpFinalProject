bb14571658438acf49d8d5ce4e07bdefea3ca8bd
Matteo Antunez <antunezmatteo@gmail.com>
2023-02-10 15:08:57 -0600

    Update Login.js

--- fitness-tracker/src/components/Login.js
+++ fitness-tracker/src/components/Login.js
@@ -8,16 +8,15 @@ return(
 
     <div>
         <form  class="form-signin w-100 m-auto">
-        {/* <img class="mb-4" src="/docs/5.3/assets/brand/bootstrap-logo.svg" alt="" width="72" height="57"/> */}
             <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
 
             <div class="form-floating">
-                <input type="text" class="form-control" id="floatingInput" placeholder="Username" name="username"/>
+                <input type="text" class="form-control" id="floatingInput" placeholder="Username"/>
                 <label for="floatingInput">Email address</label>
             </div>
 
             <div class="form-floating w-100">
-                <input type="password" class="form-control" id="floatingPassword" placeholder="Password" name="password"/>
+                <input type="password" class="form-control" id="floatingPassword" placeholder="Password"/>
                 <label for="floatingPassword">Password</label>
             </div>
 
@@ -27,7 +26,7 @@ return(
                 </label>
             </div>
 
-            <button class="w-100 btn btn-lg btn-primary" type="submit" onClick={FitnessApi.validateLogin(username, password)}>Sign in</button>
+            <button class="w-100 btn btn-lg btn-primary" type="submit" onClick={FitnessApi.validateLogin()}>Sign in</button>
             <p class="mt-5 mb-3 text-muted">©Fitness Tracker 2023</p>
         </form>
     </div>
