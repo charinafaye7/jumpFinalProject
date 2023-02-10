bde2e75bd22adb03d7d61d30342e07706ba57c53
Matteo Antunez <antunezmatteo@gmail.com>
2023-02-10 10:14:44 -0600

    Update Login.js

--- fitness-tracker/src/components/Login.js
+++ fitness-tracker/src/components/Login.js
@@ -11,23 +11,23 @@ return(
             <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
 
             <div class="form-floating">
-            <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com"/>
-            <label for="floatingInput">Email address</label>
+                <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com"/>
+                <label for="floatingInput">Email address</label>
             </div>
 
             <div class="form-floating w-100">
-            <input type="password" class="form-control" id="floatingPassword" placeholder="Password"/>
-            <label for="floatingPassword">Password</label>
+                <input type="password" class="form-control" id="floatingPassword" placeholder="Password"/>
+                <label for="floatingPassword">Password</label>
             </div>
 
             <div class="checkbox mb-3">
-            <label>
-                <input type="checkbox" value="remember-me"/> Remember me
-            </label>
+                <label>
+                    <input type="checkbox" value="remember-me"/> Remember me
+                </label>
             </div>
 
             <button class="w-100 btn btn-lg btn-primary" type="submit">Sign in</button>
-            <p class="mt-5 mb-3 text-muted">© 2023</p>
+            <p class="mt-5 mb-3 text-muted">©Fitness Tracker 2023</p>
         </form>
     </div>
 
